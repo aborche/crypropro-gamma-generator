3207e039490e9abd413cd5b3992aed3f87f170bb
aborche <zhecka@gmail.com>
2024-01-09 17:23:02 +0300

    Added autogenerated gamma files

--- genkpim.sh
+++ genkpim.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 GAMMAID=$(cat /proc/sys/kernel/random/uuid | cut -c 1-8)
-GAMMACOUNT=10
+GAMMACOUNT=250
 RANDOMBLOCKSIZE=2000
 TYPEDELAY=15
 WINDOWID=$(xdotool getwindowfocus -f)
@@ -10,8 +10,8 @@ GENKPIMBINARY=/opt/cprocsp/bin/amd64/genkpim
 (trap 'kill 0' INT TERM; sleep 2; while [ 1 ]; do echo 'Iterating'; cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-$RANDOMBLOCKSIZE} | head -n 1 | xdotool type --delay 15 --window ${WINDOWID} --file -; done; ) &
 BGPID=$!
 
-mkdir -p /tmp/genpim/${GAMMAID}
-${GENKPIMBINARY} ${GAMMACOUNT} ${GAMMAID} /tmp/genpim/${GAMMAID}
+mkdir -p /tmp/genpim/"${GAMMAID}-${GAMMACOUNT}"
+${GENKPIMBINARY} ${GAMMACOUNT} ${GAMMAID} /tmp/genpim/"${GAMMAID}-${GAMMACOUNT}"
 
 kill -9 $BGPID
 pkill -9 xdotool
